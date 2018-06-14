a75317b7a63f9d3b28faa74d71287fa83b87eb9e
Wong Horng Jir <jirwong@gmail.com>
2018-06-14 16:37:43 +0800

    Initial commit of project.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Button } from 'semantic-ui-react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            Test
+            <Button>Click Here</Button>
+        </div>
+    );
+  }
+}
+
+export default App;
