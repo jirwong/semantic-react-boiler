2b27377fd7d68a2e48fad4f0a8e6e965ac5b4039
Wong Horng Jir <jirwong@gmail.com>
2018-06-14 17:13:25 +0800

    Added prop-types

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Button } from 'semantic-ui-react';
 import './App.css';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   render() {
     return (
         <div>
             Test
-            <Button>Click Here</Button>
         </div>
     );
   }
